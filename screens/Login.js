daf9073d62fe89b3b892a9c559c5e4cef6bca5e9
Rapha-Y <raph.yuki@gmail.com>
2020-03-02 13:50:38 -0300

    added front end components in login screen

--- screens/Login.js
+++ screens/Login.js
@@ -1,11 +1,37 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, TextInput, TouchableOpacity } from 'react-native';
 
 export default class Login extends React.Component {
     render() {
         return(
             <View style={styles.container}>
-                <Text>Login</Text>
+                <Text style={styles.greeting}>{"Hello, friend!\nIt's good to see you again!"}</Text>
+
+                <View style={styles.errorMessage}>
+                    <Text>ERROR!</Text>
+                </View>
+
+                <View style={styles.form}>
+                    <View>
+                        <Text style={styles.inputTitle}>E-mail Address</Text>
+                        <TextInput style={styles.input} autoCapitalize="none"></TextInput>
+                    </View>
+
+                    <View style={{marginTop: 32}}>
+                        <Text style={styles.inputTitle}>Password</Text>
+                        <TextInput style={styles.input} secureTextEntry autoCapitalize="none"></TextInput>
+                    </View>
+                </View>
+
+                <TouchableOpacity style={styles.button}>
+                    <Text style={{color: "#FFF", fontWeight: "500"}}>Sign In</Text>
+                </TouchableOpacity>
+
+                <TouchableOpacity style={{alignSelf: "center", marginTop: 32}}>
+                    <Text style={{color: "#414959", fontSize: 13}}>
+                        First time here? <Text style={{fontWeight: "500", color: "#E9446A"}}>Sign Up</Text>
+                    </Text>
+                </TouchableOpacity>
             </View>
         );
     }
@@ -13,8 +39,43 @@ export default class Login extends React.Component {
 
 const styles = StyleSheet.create({
     container: {
-        flex: 1,
+        flex: 1
+    },
+    greeting: {
+        marginTop: 32,
+        fontSize: 18,
+        fontWeight: "400",
+        textAlign: "center"
+    },
+    errorMessage: {
+        height: 72,
+        alignItems: "center",
         justifyContent: "center",
-        alignItems: "center"
+        marginHorizontal: 30
+    },
+    form: {
+        marginBottom: 48,
+        marginHorizontal: 30
+    },
+    inputTitle: {
+        color: "#8A8F9E",
+        fontSize: 10,
+        textTransform: "uppercase"
+    },
+    input: {
+        borderBottomColor: "#8A8F9E",
+        borderBottomWidth: StyleSheet.hairlineWidth,
+        height: 40,
+        fontSize: 15,
+        color: "#161F3D"
+    
+    },
+    button: {
+        marginHorizontal: 30,
+        backgroundColor: "#E9446A",
+        borderRadius: 4,
+        height: 52,
+        alignItems: "center",
+        justifyContent: "center"
     }
 });
