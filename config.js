3499955db649e4451f980e09006286905f2a9620
Rapha-Y <raph.yuki@gmail.com>
2020-03-07 14:52:19 -0300

    added Fire.js file

---
+++ config.js
@@ -0,0 +1,9 @@
+export default {
+    apiKey: "AIzaSyAmO2SdNPrMIINFiXIq-tdiA2mJEPYLwGY",
+    authDomain: "fir-learning-1f459.firebaseapp.com",
+    databaseURL: "https://fir-learning-1f459.firebaseio.com",
+    projectId: "fir-learning-1f459",
+    storageBucket: "fir-learning-1f459.appspot.com",
+    messagingSenderId: "704551034358",
+    appId: "1:704551034358:web:b4cd12150f8c7313787bd7"
+};
