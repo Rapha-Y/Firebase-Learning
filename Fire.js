2f4d5463512573116460d3cbfed07e0fc8d7b3ef
Rapha-Y <raph.yuki@gmail.com>
2020-03-07 15:04:22 -0300

    added function to add post

--- Fire.js
+++ Fire.js
@@ -6,6 +6,54 @@ class Fire {
         firebase.initializeApp(FirebaseKeys);
     }
 
+    addPost = async ({ text, localUri }) => {
+        const remoteUri = await this.uploadPhotoAsync(localUri);
+
+        return new Promise((res, res) => {
+            this.firestore
+                .collection("posts")
+                .add({
+                    text,
+                    uid: this.uid,
+                    timestamp: this.timestamp,
+                    image: remoteUri
+                })
+                .then(ref => {
+                    res(ref);
+                })
+                .catch(error => {
+                    rej(error);
+                });
+        });
+    };
+
+    uploadPhotoAsync = async uri => {
+        const path = `photos/${this.uid}/${Date.now()}.jpg`;
+
+        return new Promise(async (res, rej) => {
+            const response = await fetch(uri);
+            const file = await response.blob();
+
+            let upload = firebase
+                .storage()
+                .ref(path)
+                .put(file);
+
+            upload.on(
+                "state_changed",
+                snapshot => {
+                    err => {
+                        rej(err);
+                    }
+                },
+                async () => {
+                    const url = await upload.snapshot.ref.getDownloadURL();
+                    res(url);
+                }
+            );
+        });
+    };
+
     get firestore() {
         return firebase.firestore();
     }
